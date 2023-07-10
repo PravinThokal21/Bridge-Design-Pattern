98b866ebb070cc41120f0fcc222c9d9a1c9e9454
Pravin Thokal <thokalpravinkumar@gmail.com>
2023-07-10 17:20:30 +0530

    added bridge design pattern

---
+++ IMessageSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Design_Pattern
+{
+    internal interface IMessageSender
+    {
+        public void SendMessage(string subject, string body);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿
+using Bridge_Design_Pattern;
+
 Console.WriteLine("Hello, World!");
+
+IMessageSender emailSender = new EmailSender();
+IMessageSender msmqSender = new MSMQSender();
+IMessageSender webSender = new WebServiceSender();
+
+Message message = new SystemMessage();
+message.Subject = "Test Message";
+message.Body = "Hi, This is a Test Message";
+
+message.MessageSender = emailSender;
+message.Send();
+
+message.MessageSender = webSender;
+message.Send();
+
+message.MessageSender = msmqSender;
+message.Send();
+
+UserMessage usermsg = new UserMessage();
+usermsg.Subject = "Test Message";
+usermsg.Body = "Hi, This is a Test Message";
+usermsg.UserComments = "I hope you are well";
+
+usermsg.MessageSender = emailSender;
+usermsg.Send();
+
+usermsg.MessageSender = webSender;
+usermsg.Send();
+
+usermsg.MessageSender = msmqSender;
+usermsg.Send();
